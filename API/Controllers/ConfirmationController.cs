c8cab5939dbf1f0bbeb37e560037e8f23643de86
Joaaker <joakim.akerman@hotmail.com>
2025-11-10 10:26:00 +0100

    Added controller

---
+++ API/Controllers/ConfirmationController.cs
@@ -0,0 +1,24 @@
+﻿using API.Interfaces;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ConfirmationController(IConfirmationService service) : ControllerBase
+{
+    private readonly IConfirmationService _service = service;
+
+    [HttpPost("send-confirmation-email")]
+    public async Task<IActionResult> SendConfirmationEmail(ConfirmationRequest request)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest("ModelState not valid");
+
+        var result = await _service.SendConfirmationEmailAsync(request);
+        return result.Succeeded
+            ? Accepted(result)
+            : BadRequest(result);
+    }
+}
