fcb90b3ff02f9b101cb9a959c33873a10f36108e
Joaaker <joakim.akerman@hotmail.com>
2025-11-11 10:47:51 +0100

    Added Request model

--- API/Models/ConfirmationRequest.cs
+++ API/Models/ConfirmationRequest.cs
@@ -2,4 +2,10 @@
 
 public class ConfirmationRequest
 {
+    public string FormType { get; set; } = null!;
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Question { get; set; }
+    public string? SelectedOption { get; set; }
 }
